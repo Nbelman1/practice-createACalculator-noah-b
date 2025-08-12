2addab6b2ccbdff271f26b35c2a3ade7774692e9
Noah <nbelman1@gmail.com>
2025-08-12 13:23:23 -0400

    Debug calculator functions, create basic function to run program

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
+const input = require('readline-sync');
+
 /* 
-● Absolute Value Calculation: Given any number, return its absolute
-value.
-● Power Calculation: Calculate and return the value of a base raised to
-a specific power.
-● Square Root Finder: Calculate the square root of a number.
-● Maximum and Minimum Finder: From a given set of numbers,
-determine the largest and smallest values.
-● Random Number Generator: Generate a random integer within a
-specified range.
-● Custom Rounding: Round a number to a specified number of decimal
-places.
+Find the absolute value of -45.67.
+● Raise 5 to the power of 3.
+● Calculate the square root of 144.
+● Determine the largest and smallest values from [3, 78, -12,
+0.5, 27].
+● Generate a random number between 1 and 50.
+1
+● Round 23.67891 to 2 decimal places.
 */
 
 // Arrays for testing
@@ -19,7 +18,7 @@ let arr3 = [400, 200, -100, 0];
 
 
 // Declaring functions for user options
-
+// TODO: add user input to each function
 function calcAbsValue(num) {
     return Math.abs(num);
 }
@@ -62,17 +61,14 @@ function findRandomInt(arr) {
     return arr[index];
 }
 
-// TODO: round a number to a specified number of decimals 
-// fix bug, function not working
 function roundToDecimals(numToRound, numDecimals) {
     let factor = createDecimalFactor(numDecimals);
-    let product = numToRound * factor;
-    Math.round(product);
+    let product = Math.round(numToRound * factor);
     let result = product / factor;
     return result;
 }
 
-function createDecimalFactor(input) { // creates a number 1 concatenated with specified number of 0's 
+function createDecimalFactor(input) { // creates a number: 1 concatenated with specified number of 0's 
     let factor = "1"; // we also could raise 10 to the power of input 
     for (i = 0; i < input; i++) {
         factor += "0";
@@ -80,8 +76,50 @@ function createDecimalFactor(input) { // creates a number 1 concatenated with sp
     return Number(factor); 
 }
 
-// test case 
-console.log(roundToDecimals(5.98384, 2));
-
 // TODO: function that runs the program
+function runProgram() {
+    displayOptions();
+    readUserInput();
+}
 
+function displayOptions() {
+    console.log("Welcome to Create a Calculator!\n");
+    console.log("To perform an operation, please enter the associated number.");
+    console.log("Operation              Number");
+    console.log("Absolute value         1");
+    console.log("Raise to power         2");
+    console.log("Square root            3");
+    console.log("Find max               4");
+    console.log("Find min               5");
+    console.log("Find max and min       6");
+    console.log("Choose random number   7");
+    console.log("Round number           8");
+}
+
+function readUserInput(choice) {
+    let isRunning = true;
+    choice = input.questionInt("Enter a number here: ");
+    while (isRunning) {
+        if (choice === 1) {
+            calcAbsValue();
+        } else if (choice === 2) {
+            raiseToPower();
+        } else if (choice === 3) {
+            calcSqRoot();
+        } else if (choice === 4) {
+            findMax();
+        } else if (choice === 5) {
+            findMin();
+        } else if (choice === 6) {
+            findMaxAndMin();
+        } else if (choice === 7) {
+            findRandomInt();
+        } else if (choice === 8) {
+            roundToDecimals();
+        } else if (choice < 1 || choice > 8) {
+            input.questionInt("Invalid selection. Enter a number here: "); // TODO: connect to rest of while loop
+        }
+    }
+}
+
+runProgram();
